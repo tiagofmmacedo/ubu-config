a47c76770ed426d8e9c9bf02d9c95527a190a45f
Tiago Macedo <tiago.macedo@ceiia.com>
2022-06-30 17:55:43 +0100

    add git clone script

---
+++ git_clone.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+cd ~/workspace
+git clone git@gitlab.ceiia.com:ees/general/hardware.git
+git clone git@gitlab.ceiia.com:ees/general/hardware.wiki.git
+git clone git@gitlab.ceiia.com:ees/general/software.git
+git clone git@gitlab.ceiia.com:ees/general/software.wiki.git
+git clone git@gitlab.ceiia.com:ees/general/documentation.git
+git clone git@gitlab.ceiia.com:ees/general/documentation.wiki.git
+git clone git@gitlab.ceiia.com:ees/es/aquasys.git
+git clone git@gitlab.ceiia.com:ees/es/aquasys.wiki.git
+git clone git@gitlab.ceiia.com:ees/es/bikes.git
+git clone git@gitlab.ceiia.com:ees/es/bikes.wiki.git
+git clone git@gitlab.ceiia.com:ees/es/ctech.git
+git clone git@gitlab.ceiia.com:ees/es/ctech.wiki.git
+git clone git@gitlab.ceiia.com:ees/es/evio.git
+git clone git@gitlab.ceiia.com:ees/es/evio.wiki.git
+git clone git@gitlab.ceiia.com:ees/es/ghisallo.git
+git clone git@gitlab.ceiia.com:ees/es/ghisallo.wiki.git
+git clone git@gitlab.ceiia.com:ees/es/l4s.git
+git clone git@gitlab.ceiia.com:ees/es/l4s.wiki.git
+git clone git@gitlab.ceiia.com:ees/es/sec.git
+git clone git@gitlab.ceiia.com:ees/es/sec.wiki.git
+git clone git@gitlab.ceiia.com:ees/es/tags.git
+git clone git@gitlab.ceiia.com:ees/es/tags.wiki.git
+git clone git@gitlab.ceiia.com:ees/es/umdc.git
+git clone git@gitlab.ceiia.com:ees/es/umdc.wiki.git
+git clone git@gitlab.ceiia.com:ees/es/wrs.git
+git clone git@gitlab.ceiia.com:ees/es/wrs.wiki.git
